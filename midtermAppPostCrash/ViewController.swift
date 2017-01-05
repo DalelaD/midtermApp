aa77add8dbd865319db210352aeab0b2d84b49c9
ZD <dalezachary4@gmail.com>
2017-01-04 22:41:16 -0500

    Table view added but was unable to link table view to my code

--- midtermAppPostCrash/ViewController.swift
+++ midtermAppPostCrash/ViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+  
+    @IBAction func login(_ sender: AnyObject) {
+        performSegue(withIdentifier: "login", sender: "continue")
+        print("Logging User in")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
