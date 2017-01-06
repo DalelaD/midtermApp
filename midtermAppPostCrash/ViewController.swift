b7b513414ff32c24d87617c0eb10a6b118eaaa16
ZD <dalezachary4@gmail.com>
2017-01-06 08:20:10 -0500

    commit 4 ( up to sides page completed, need to add action to the finish button, added table view and coded it, is working at this point)

--- midtermAppPostCrash/ViewController.swift
+++ midtermAppPostCrash/ViewController.swift
@@ -8,9 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController{
-
-  
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate{
+    
+    @IBOutlet weak var dayTable: UITableView!
+    
+    var dayTbl=[ "Monday","Tuesday","Wednesday","Thursday","Friday"]
+    
+    //Sets Table info
+    
     @IBAction func login(_ sender: AnyObject) {
         performSegue(withIdentifier: "login", sender: "continue")
         print("Logging User in")
@@ -21,8 +26,32 @@ class ViewController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        dayTable.dataSource=self
 
+        dayTable.delegate=self
+        // ^ builds the data source within the code and used to define the table
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return dayTbl.count
+    }
+    //^ counts the number of things in the array
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        print(indexPath.row)
+        
+        let cell=UITableViewCell()
+        //^ same as assigning a value
+        
+        cell.textLabel?.text=dayTbl[indexPath.row]
+        
+        return cell
+    }
+ //^ says in order what do you want in the cells
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
